b1803a84872aa78a584cc406dba1140bd0b316f5
Shiba Kar <shibakar@swazei.com>
2023-08-12 15:52:23 +0530

    added partner_id

--- lib/src/model/user_logged_in.dart
+++ lib/src/model/user_logged_in.dart
@@ -12,23 +12,26 @@ class UserLoggedIn {
   final bool is_admin;
   final UserContext user_context;
   final String db;
+  final int partner_id;
   final String server_version;
   final String name;
   final String username;
   final UserCompanies user_companies;
   final Map<String, dynamic> currencies;
 
-  UserLoggedIn(
-      {required this.uid,
-      required this.is_system,
-      required this.is_admin,
-      required this.user_context,
-      required this.db,
-      required this.server_version,
-      required this.name,
-      required this.username,
-      required this.user_companies,
-      required this.currencies});
+  UserLoggedIn({
+    required this.uid,
+    required this.is_system,
+    required this.is_admin,
+    required this.user_context,
+    required this.db,
+    required this.server_version,
+    required this.name,
+    required this.username,
+    required this.partner_id,
+    required this.user_companies,
+    required this.currencies,
+  });
   factory UserLoggedIn.fromJson(Map<String, dynamic> json) =>
       _$UserLoggedInFromJson(json);
   Map<String, dynamic> toJson() => _$UserLoggedInToJson(this);
